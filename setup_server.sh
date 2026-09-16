5fa73148e97d0cd56bb11a70c8cb6d97cfcfc0ae
varun0406 <kotwaniv03@gmail.com>
2026-09-16 14:19:04 +0530

    Force postgres to update password

--- setup_server.sh
+++ setup_server.sh
@@ -42,6 +42,7 @@ DB_NAME="greenpark_inventory"
 # Run postgres commands as the postgres user to create db and user
 sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;" || true
 sudo -u postgres psql -c "CREATE USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASS';" || true
+sudo -u postgres psql -c "ALTER USER $DB_USER WITH ENCRYPTED PASSWORD '$DB_PASS';" || true
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;" || true
 sudo -u postgres psql -d $DB_NAME -c "GRANT ALL ON SCHEMA public TO $DB_USER;" || true
 
